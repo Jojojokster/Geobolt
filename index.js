6d4372647c64752a30b2b7c7eb107afdfb970728
Joseph <josephajayiogunya@gmail.com>
2024-12-04 05:10:32 +0000

    Modified index.js deleted app.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 // Import the modules we need
 const express = require('express');
 const ejs = require('ejs');
@@ -8,9 +12,11 @@ const session = require('express-session');
 const passport = require('passport');
 const path = require('path');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const initializePassport = require('./passport-config');
 
 // Load environment variables from .env file
-dotenv.config();
+dotenv.config({ path: './.env' });
 
 // Create the express application object
 const app = express();
@@ -21,10 +27,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
+app.use(cookieParser());
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24, httpOnly: true // 86400000 1 day
+    }
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -49,8 +59,26 @@ const db = mysql.createPool({
 // Make the database connection available globally
 global.db = db;
 
-// Passport configuration
-require('./passport-config')(passport);
+db.getConnection()
+    .then(() => {
+        console.log('Connected to database');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+// Initialize Passport
+initializePassport(
+    passport, 
+    async email => {
+        const [results] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
+        return results.length > 0 ? results[0] : null;
+    },
+    async id => {
+        const [results] = await db.query('SELECT * FROM users WHERE id = ?', [id]);
+        return results.length > 0 ? results[0] : null;
+    }
+);
 
 // Routes
 const authRoutes = require('./routes/auth');
@@ -59,5 +87,19 @@ const pageRoutes = require('./routes/pages');
 app.use('/auth', authRoutes);
 app.use('/', pageRoutes);
 
+// Serve uploaded files
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.get('/', (req, res) => {
+    res.render("index", { messages: req.flash(), user: req.user });
+});
+
+app.post("/logout", (req, res) => {
+    req.logout(err => {
+        if (err) return next(err);
+        res.redirect("/");
+    });
+});
+
 // Start the web app listening
-app.listen(port, () => console.log(`Geobolt app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
